0a75f40f2caf392c3f1d93ca0bd6526ca1bd31a9
Emona Santiago <emonasantiago1207@gmail.com>
2023-01-25 13:21:18 +0100

    random images

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<section id="homepage">
+  <h1>SCROLL</h1>
+  <img
+    class="background"
+    src="src/images/Schermafbeelding 2023-01-25 om 12.54.27.png"
+    alt="background"
+    style="width:100%;"
+  />
+
+  <img class="pop-left" src="src/images/klipartz.com.png" alt="pop-left" />
+  <img
+    class="pop-right"
+    src="src/images/—Pngtree—cartoon cute explosion dialog bubble_4040022.png"
+    alt="pop-right"
+  />
+
+  <img class="pop-center" src="src/images/pngegg (3).png" alt="pop-center" />
+</section>
+
+<style>
+  * {
+    scroll-behavior: smooth;
+  }
+
+  h1 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .pop-left {
+    position: absolute;
+    top: -300px;
+    left: -300px;
+    width: 60em;
+  }
+
+  .pop-right {
+    position: absolute;
+    bottom: -700px;
+    right: -500px;
+    width: 80em;
+  }
+
+  .pop-center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 20em;
+  }
+</style>
